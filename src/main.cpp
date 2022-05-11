cb2d7d283eba643b30d6bcef9d90797c302c884e
Radosław Radomski <48625686+RescueRangers@users.noreply.github.com>
2022-05-11 14:57:41 +0200

    First refactor

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,15 @@
 #include <Bounce.h>
 
 #define START_PIN 20
-
-#define program1 2
-#define program2 3
-#define program3 4
-#define program4 5
-#define program5 6
-#define program6 7
-#define program7 8
-#define program8 9
-#define program9 10
+#define PROGRAM_PIN1 2
+#define PROGRAM_PIN2 3
+#define PROGRAM_PIN3 4
+#define PROGRAM_PIN4 5
+#define PROGRAM_PIN5 6
+#define PROGRAM_PIN6 7
+#define PROGRAM_PIN7 8
+#define PROGRAM_PIN8 9
+#define PROGRAM_PIN9 10
 #define PAUSE_PIN 11
 #define STOP_PIN 12
 
@@ -27,93 +26,205 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-// Deffine buttons used for program selection
-Bounce program1Button = Bounce(program1, 10);
-Bounce program2Button = Bounce(program2, 10);
-Bounce program3Button = Bounce(program3, 10);
-Bounce program4Button = Bounce(program4, 10);
-Bounce program5Button = Bounce(program5, 10);
-Bounce program6Button = Bounce(program6, 10);
-Bounce program7Button = Bounce(program7, 10);
-Bounce program8Button = Bounce(program8, 10);
-Bounce program9Button = Bounce(program9, 10);
-Bounce startButton = Bounce(START_PIN, 10);
-Bounce pauseButton = Bounce(PAUSE_PIN, 10);
-Bounce stopButton = Bounce(STOP_PIN, 10);
-
-int minDelay = 17;
-int analogWait = 0;
-uint32_t color = strip.Color(10, 0, 0);
-uint32_t blank = strip.Color(0, 0, 0);
-bool isOffset = true;
-bool isPixels = false;
-bool pattern[PIXEL_COUNT];
-int currentOffset = 0;
-bool show = true;
-bool paused = false;
-
-// Fill the pattern array with pixels and pixel offset
-void fillPattern(int pixels, int pixelOffset)
+// Pattern types supported:
+enum program
 {
-  for (int j = 0; j < strip.numPixels(); j += (pixels + pixelOffset))
+  NONE,
+  PROGRAM1,
+  PROGRAM2,
+  PROGRAM3,
+  PROGRAM4,
+  PROGRAM5,
+  PROGRAM6,
+  PROGRAM7,
+  PROGRAM8,
+  PROGRAM9
+};
+// Pattern directions supported:
+enum direction
+{
+  FORWARD,
+  REVERSE
+};
+void ProgramComplete();
+class NeoPatterns : public Adafruit_NeoPixel
+{
+public:
+  // Member Variables:
+  program ActivePattern; // which pattern is running
+  direction Direction;   // direction to run the pattern
+
+  unsigned long Interval;   // milliseconds between updates
+  unsigned long lastUpdate; // last update of position
+
+  uint32_t Color1, Color2; // What colors are in use
+  uint16_t TotalSteps;     // total number of steps in the pattern
+  uint16_t Index;          // current step within the pattern
+
+  void (*OnComplete)(); // Callback on completion of pattern
+
+  bool isOffset = true;
+  bool isPixels = false;
+  bool *PatternPtr = new bool[TotalSteps];
+  int currentOffset = 0;
+  uint16_t pixel, pixelOffset;
+  uint32_t color;
+  uint32_t blank;
+
+  // Constructor - calls base-class constructor to initialize strip
+  NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type, void (*callback)())
+      : Adafruit_NeoPixel(pixels, pin, type)
   {
-    for (int i = 0; i < pixels; i++)
+    TotalSteps = pixels;
+    PatternPtr = new bool[pixels];
+    color = Color(10, 0, 0);
+    blank = Color(0, 0, 0);
+    OnComplete = callback;
+  }
+
+  // Update the pattern
+  void Update()
+  {
+    if ((millis() - lastUpdate) > Interval) // time to update
     {
-      pattern[i + j] = 1;
+      lastUpdate = millis();
+      switch (ActivePattern)
+      {
+      case PROGRAM1:
+        Cycle(36, 36);
+        break;
+      case PROGRAM2:
+        Cycle(48, 36);
+        break;
+      case PROGRAM3:
+        Cycle(36, 60);
+        break;
+      case PROGRAM4:
+        Cycle(48, 60);
+        break;
+      case PROGRAM5:
+        Cycle(60, 60);
+        break;
+      case PROGRAM6:
+        Cycle(48, 84);
+        break;
+      case PROGRAM7:
+        Cycle(60, 84);
+        break;
+      case PROGRAM8:
+        Cycle(72, 84);
+        break;
+      case PROGRAM9:
+        Cycle(60, 108);
+        break;
+      default:
+        break;
+      }
     }
-    for (int i = 0; i < pixelOffset; i++)
+  }
+
+  // Increment the Index and reset at the end
+  void Increment()
+  {
+    if (Direction == FORWARD)
     {
-      pattern[i + j + pixels] = 0;
+      Index++;
+      if (Index >= TotalSteps)
+      {
+        Index = 0;
+        if (OnComplete != NULL)
+        {
+          OnComplete(); // call the completion callback
+        }
+      }
+    }
+    else // Direction == REVERSE
+    {
+      --Index;
+      if (Index <= 0)
+      {
+        Index = TotalSteps - 1;
+        if (OnComplete != NULL)
+        {
+          OnComplete(); // call the completion callback
+        }
+      }
     }
   }
-}
 
-// Shift the array by one frame depending on the pixels and pixel offset
-void shiftArray(int pixels, int pixelOffset)
-{
-  int currentColor;
-  if (isOffset && currentOffset == pixelOffset)
+  // Reverse pattern direction
+  void Reverse()
   {
-    currentOffset = 0;
-    isOffset = false;
-    isPixels = true;
-  }
-  if (isPixels && currentOffset == pixels)
-  {
-    currentOffset = 0;
-    isOffset = true;
-    isPixels = false;
+    if (Direction == FORWARD)
+    {
+      Direction = REVERSE;
+      Index = TotalSteps - 1;
+    }
+    else
+    {
+      Direction = FORWARD;
+      Index = 0;
+    }
   }
 
-  if (isOffset)
+  void ShiftArray(int pixels, int pixelOffset)
   {
-    currentColor = 0;
-    currentOffset++;
+    int currentColor;
+    if (isOffset && currentOffset == pixelOffset)
+    {
+      currentOffset = 0;
+      isOffset = false;
+      isPixels = true;
+    }
+    if (isPixels && currentOffset == pixels)
+    {
+      currentOffset = 0;
+      isOffset = true;
+      isPixels = false;
+    }
+
+    if (isOffset)
+    {
+      currentColor = 0;
+      currentOffset++;
+    }
+
+    if (isPixels)
+    {
+      currentColor = 1;
+      currentOffset++;
+    }
+
+    for (int i = TotalSteps; i >= 1; i--)
+    {
+      PatternPtr[i] = PatternPtr[i - 1];
+    }
+    PatternPtr[0] = currentColor;
   }
 
-  if (isPixels)
+  void FillPattern(int pixels, int pixelOffset)
   {
-    currentColor = 1;
-    currentOffset++;
+    for (int j = 0; j < TotalSteps; j += (pixels + pixelOffset))
+    {
+      for (int i = 0; i < pixels; i++)
+      {
+        PatternPtr[i + j] = 1;
+      }
+      for (int i = 0; i < pixelOffset; i++)
+      {
+        PatternPtr[i + j + pixels] = 0;
+      }
+    }
   }
 
-  for (int i = PIXEL_COUNT; i >= 1; i--)
+  // Cycle through the animation frames
+  void Cycle(int pixels, int pixelOffset)
   {
-    pattern[i] = pattern[i - 1];
-  }
-  pattern[0] = currentColor;
-}
+    FillPattern(pixels, pixelOffset);
 
-// Cycle through the animation frames
-void cycle(int pixels, int pixelOffset)
-{
-  fillPattern(pixels, pixelOffset);
+    // First show the pattern without starting the animation until the start button is pressed
 
-  // First show the pattern without starting the animation until the start button is pressed
-  while (show)
-  {
     int colorIntensity = 0;
     for (size_t i = 0; i < 32; i++)
     {
@@ -121,11 +232,11 @@ void cycle(int pixels, int pixelOffset)
     }
 
     colorIntensity = ((colorIntensity / 32) >> 2) - 10;
-    color = strip.Color(colorIntensity + 10, 0, 0);
-    for (int j = 0; j < PIXEL_COUNT; j++)
+    color = Color(colorIntensity + 10, 0, 0);
+    for (int j = 0; j < TotalSteps; j++)
     {
       uint32_t currentColor;
-      if (pattern[j % PIXEL_COUNT])
+      if (PatternPtr[j % TotalSteps])
       {
         currentColor = color;
       }
@@ -133,168 +244,189 @@ void cycle(int pixels, int pixelOffset)
       {
         currentColor = blank;
       }
-      strip.setPixelColor(j, currentColor);
+      setPixelColor(j, currentColor);
     }
-    strip.show();
-
-    if (startButton.update())
-    {
-      if (startButton.fallingEdge())
-      {
-        show = false;
-      }
-    }
+    show();
   }
 
-  // Run the animation until it gets paused by user input
-  while (!paused)
+  // Calculate 50% dimmed version of a color (used by ScannerUpdate)
+  uint32_t DimColor(uint32_t color)
   {
-    for (int i = 0; i < (pixels + pixelOffset); i++)
-    {
-      int colorIntensity = 0;
-      for (size_t i = 0; i < 32; i++)
-      {
-        colorIntensity += analogRead(1);
-      }
-
-      colorIntensity = ((colorIntensity / 32) >> 2) - 10;
-      color = strip.Color(colorIntensity + 10, 0, 0);
-      for (int j = 0; j < PIXEL_COUNT; j++)
-      {
-        uint32_t currentColor;
-        if (pattern[j % PIXEL_COUNT])
-        {
-          currentColor = color;
-        }
-        else
-        {
-          currentColor = blank;
-        }
-        strip.setPixelColor(j, currentColor);
-      }
-      strip.show();
-      shiftArray(pixels, pixelOffset);
-
-      for (size_t i = 0; i < 32; i++)
-      {
-        analogWait += analogRead(0);
-      }
-
-      analogWait = analogWait / 32;
-
-      if (pauseButton.update())
-      {
-        if (pauseButton.fallingEdge())
-        {
-          paused = true;
-        }
-      }
-      delay(analogWait + minDelay);
-    }
+    // Shift R, G and B components one bit to the right
+    uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
+    return dimColor;
   }
-  while (paused)
+
+  // Set all pixels to a color (synchronously)
+  void ColorSet(uint32_t color)
   {
-    if (pauseButton.update())
+    for (int i = 0; i < numPixels(); i++)
     {
-      if (pauseButton.fallingEdge())
-      {
-        paused = false;
-      }
+      setPixelColor(i, color);
     }
-    if (stopButton.update())
+    show();
+  }
+
+  // Returns the Red component of a 32-bit color
+  uint8_t Red(uint32_t color)
+  {
+    return (color >> 16) & 0xFF;
+  }
+
+  // Returns the Green component of a 32-bit color
+  uint8_t Green(uint32_t color)
+  {
+    return (color >> 8) & 0xFF;
+  }
+
+  // Returns the Blue component of a 32-bit color
+  uint8_t Blue(uint32_t color)
+  {
+    return color & 0xFF;
+  }
+
+  // Input a value 0 to 255 to get a color value.
+  // The colours are a transition r - g - b - back to r.
+  uint32_t Wheel(byte WheelPos)
+  {
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85)
     {
-      if (stopButton.fallingEdge())
-      {
-        paused = false;
-        show = true;
-        return;
-      }
+      return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    else if (WheelPos < 170)
+    {
+      WheelPos -= 85;
+      return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    else
+    {
+      WheelPos -= 170;
+      return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
     }
   }
-}
+};
+
+NeoPatterns Program(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800, &ProgramComplete);
+
+// Deffine buttons used for program selection
+Bounce program1Button = Bounce(PROGRAM_PIN1, 10);
+Bounce program2Button = Bounce(PROGRAM_PIN2, 10);
+Bounce program3Button = Bounce(PROGRAM_PIN3, 10);
+Bounce program4Button = Bounce(PROGRAM_PIN4, 10);
+Bounce program5Button = Bounce(PROGRAM_PIN5, 10);
+Bounce program6Button = Bounce(PROGRAM_PIN6, 10);
+Bounce program7Button = Bounce(PROGRAM_PIN7, 10);
+Bounce program8Button = Bounce(PROGRAM_PIN8, 10);
+Bounce program9Button = Bounce(PROGRAM_PIN9, 10);
+Bounce startButton = Bounce(START_PIN, 10);
+Bounce pauseButton = Bounce(PAUSE_PIN, 10);
+Bounce stopButton = Bounce(STOP_PIN, 10);
+
+int minDelay = 17;
+int analogWait = 0;
+bool isOffset = true;
+bool isPixels = false;
+bool pattern[PIXEL_COUNT];
+int currentOffset = 0;
+bool stopped = true;
+bool paused = false;
 
 void setup()
 {
   // put your setup code here, to run once:
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
   pinMode(START_PIN, INPUT_PULLUP);
-  pinMode(program1, INPUT_PULLUP);
-  pinMode(program2, INPUT_PULLUP);
-  pinMode(program3, INPUT_PULLUP);
-  pinMode(program4, INPUT_PULLUP);
-  pinMode(program5, INPUT_PULLUP);
-  pinMode(program6, INPUT_PULLUP);
-  pinMode(program7, INPUT_PULLUP);
-  pinMode(program8, INPUT_PULLUP);
-  pinMode(program9, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN1, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN2, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN3, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN4, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN5, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN6, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN7, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN8, INPUT_PULLUP);
+  pinMode(PROGRAM_PIN9, INPUT_PULLUP);
   pinMode(PAUSE_PIN, INPUT_PULLUP);
+  pinMode(STOP_PIN, INPUT_PULLUP);
+  Program.begin();
 }
 
 void loop()
 {
-  // Check for the program to run
-  if (program1Button.update())
+  if (!paused)
+    Program.Update();
+
+  if (pauseButton.update())
   {
-    if (program1Button.fallingEdge())
+    if (pauseButton.fallingEdge())
     {
-      cycle(36, 36);
+      paused = !paused;
     }
   }
-  if (program2Button.update())
+  if (!stopped)
   {
-    if (program2Button.fallingEdge())
+    // Check for the program to run
+    if (program1Button.update())
     {
-      cycle(48, 36);
+      if (program1Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM1;
+      }
     }
-  }
-  if (program3Button.update())
-  {
-    if (program3Button.fallingEdge())
+    if (program2Button.update())
     {
-      cycle(36, 60);
+      if (program2Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM2;
+      }
     }
-  }
-  if (program4Button.update())
-  {
-    if (program4Button.fallingEdge())
+    if (program3Button.update())
     {
-      cycle(48, 60);
+      if (program3Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM3;
+      }
     }
-  }
-  if (program5Button.update())
-  {
-    if (program5Button.fallingEdge())
+    if (program4Button.update())
     {
-      cycle(60, 60);
+      if (program4Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM4;
+      }
     }
-  }
-  if (program6Button.update())
-  {
-    if (program6Button.fallingEdge())
+    if (program5Button.update())
     {
-      cycle(48, 84);
+      if (program5Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM5;
+      }
     }
-  }
-  if (program7Button.update())
-  {
-    if (program7Button.fallingEdge())
+    if (program6Button.update())
     {
-      cycle(60, 84);
+      if (program6Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM6;
+      }
     }
-  }
-  if (program8Button.update())
-  {
-    if (program8Button.fallingEdge())
+    if (program7Button.update())
     {
-      cycle(72, 84);
+      if (program7Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM7;
+      }
     }
-  }
-  if (program9Button.update())
-  {
-    if (program9Button.fallingEdge())
+    if (program8Button.update())
     {
-      cycle(60, 108);
+      if (program8Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM8;
+      }
+    }
+    if (program9Button.update())
+    {
+      if (program9Button.fallingEdge())
+      {
+        Program.ActivePattern = PROGRAM9;
+      }
     }
   }
 }
